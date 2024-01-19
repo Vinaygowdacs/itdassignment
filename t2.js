8006f3e788bcae1a9aa7f88f5cbc587fbc3d46fa
vinaycs <vinaycs4242@gmail.com>
2024-01-19 23:18:51 +0530

    practise

---
+++ t2.js
@@ -0,0 +1,74 @@
+ // let data1 = "my NAME is Vinay and i am working in TCS"
+//console.log(data1.slice(5,10))
+//console.log(data1.repeat(5))
+
+
+// let data2 = `Hello ${data1}`
+
+// console.log(data1)
+// console.log(data2)
+
+// let a= 10
+// let b = 20
+// let sum= a+b
+// console.log(`the sum of ${a} and ${b} is ${sum}`)
+
+
+// let a=`Hi`
+// let b=`js`
+// console.log (`${a} ${b}`)
+
+ // console.log(data1.length)
+
+ // console.log(`the length of data1 is ${data1.length}`)
+
+ // console.log(data1.toUpperCase())
+ // console.log(data1.toLowerCase())
+
+ // console.log(data1.substr(5,40))
+
+ // console.log(data1.substring(5,10))
+
+ // console.log(data1.substring(10,5))
+
+
+// let data1 = "my NAME is Vinay and i am working in TCS"
+// console.log(data1.split())
+// console.log(data1.split(``))
+// console.log(data1.split(' '))
+// console.log(data1.split(`V`))
+
+
+// let data = `           vinay          `
+// console.log(data.trim())
+// console.log(data.trimEnd())
+// console.log(data.trimStart())
+// console.log(data.valueOf())
+// let data1 = "my NAME is Vinay and i am working in TCS"
+// console.log(data1.includes(`Vinay`))
+// console.log(data1.includes(`Vanitha`))
+
+// console.log(data1.toUpperCase().includes(`TCS`))
+
+// console.log(data1.toLowerCase().includes(`MY`))
+
+
+// let data = "my NAME is Vinay and i am working in TCS"
+// console.log(data.indexOf('NAME'))
+// console.log(data.lastIndexOf('NAME'))
+
+
+// console.log(data.replace('NAME','name'))
+
+// console.log(data.charAt(10))
+
+
+// console.log(data.charAt(8))
+
+// let fn='vinay'
+// let ln='c s'
+// console.log(fn.concat(ln))
+// console.log(fn.concat(" ",ln))
+
+let a= '202'
+console.log(a.padStart(5, '0'))
