7068db258edaaa49064de58c846089805543f2d7
vinaycs <vinaycs4242@gmail.com>
2024-01-25 00:37:19 +0530

    examples

---
+++ d24.js
@@ -0,0 +1,49 @@
+// let a =[1,2,3]
+// console.log(a)
+// let b= a.push(4)
+// console.log(a)
+
+
+// let a = "vinay"
+// console.log(a.split(""))
+
+
+// let name=['vinay','dhanu','hemu']
+// console.log(name)
+// name.unshift('sumanth')
+// console.log(name)
+
+// let name1=['vinay','dhanu','hemu']
+// console.log(name1)
+// name1.push('sumanth')
+// console.log(name1)
+
+
+// let mobile=['iphone','mi','oppo','vivo']
+// console.log(mobile)
+// mobile.pop()
+// console.log(mobile)
+
+
+// let mob=['iphone','mi','oppo','vivo']
+// console.log(mob)
+// mob.shift()
+// console.log(mob)
+
+
+// let a=[1,2,3,4,5]
+// console.log(a)
+// a[3]=25
+// console.log(a)
+
+// let a=[1,2,3,4,5]
+// console.log(a)
+// a.splice(1,2,6,8,7,6,10)
+// console.log(a)
+
+
+
+let cricteam=['ind','aus','eng','afg','nep','pak']
+console.log(cricteam)
+cricteam.splice(3,2,'sl','nz')
+console.log(cricteam)
